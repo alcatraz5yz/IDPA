4983dbd0c3111b3670c9decf3191afef0338a639
Allan Küng <2012pkual@gmail.com>
2017-04-24 21:07:53 +0200

    d

--- bunnygame/Boot.js
+++ bunnygame/Boot.js
@@ -4,10 +4,10 @@ BunnyDefender.Boot = function(game) {};
 
 BunnyDefender.Boot.prototype = {
     preload: function() {
-        this.load.image('preloaderBar', 'images/loader_bar.png');
-        this.load.image('titleimage', 'images/TitleImage.png');
+        this.load.image('preloaderBar', '../images/loader_bar.png');
+        this.load.image('titleimage', '../images/TitleImage.png');
     },
-    
+
     create: function() {
         this.input.maxPointers = 1;
 		this.stage.disableVisibilityChange = false;
@@ -21,7 +21,7 @@ BunnyDefender.Boot.prototype = {
 
 		this.input.addPointer();
 		this.stage.backgroundColor = '#171642';
-        
+
         this.state.start('Preloader');
     }
 }
